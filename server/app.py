a72d4c33241e503ccc31512271248febcc2c2657
Henry Yun <henrysungyun@gmail.com>
2023-04-16 23:12:39 -0700

    finish

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, jsonify, session
+from flask import Flask, make_response, jsonify, session, request
 from flask_migrate import Migrate
 
 from models import db, Article, User
@@ -27,8 +27,19 @@ def index_articles():
 
 @app.route('/articles/<int:id>')
 def show_article(id):
+    if 'page_views' not in session:
+        session['page_views'] = 0
 
-    pass
+    session['page_views'] += 1
+    if session['page_views'] <= 3:
+        article = Article.query.filter_by(id=id).first()
+        if article:
+            return article.to_dict()
+        else:
+            return {'message': 'Article not found'}, 404
+    else:
+        return {'message': 'Maximum pageview limit reached'}, 401
+
 
 if __name__ == '__main__':
     app.run(port=5555)
